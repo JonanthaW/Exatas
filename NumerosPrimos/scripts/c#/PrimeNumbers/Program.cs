cc5f1ad1a83b096c758c1538a538c5c4a97fc761
jonanthaW <jonanthawillian@gmail.com>
2023-02-21 00:16:45 -0300

    Added: script for c# / Prime numbers

---
+++ NumerosPrimos/scripts/c#/PrimeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PrimeNumbers {
+    public class Program {
+        static void Main(string[] args) {
+
+            int low = 0;
+            int high = 1000;
+
+            for (int i = low; i <= high; i++) {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0) {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0 && i != 1) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
